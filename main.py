e5231855a81cf4a54a2656f027ae3c95d115275f
gaurja-babbar-30078206 <gaurja.babbar@adani.com>
2024-05-14 18:08:10 +0530

    dfdf

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("MediumOrchid1")
+timmy.forward(100)
+
+print(timmy)
+
+my_screen = Screen()
+my_screen.exitonclick()
+my_screen.canvwidth(20)
